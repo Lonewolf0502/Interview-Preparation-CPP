ca3b852469b2a94bd76cf80abf8405f345e3474b
Aditya Dwivedi <106549312+Lonewolf0502@users.noreply.github.com>
2022-11-26 12:32:10 +0530

    Update NoRepeatSubstring.cpp
    Solution using map

--- NoRepeatSubstring.cpp
+++ NoRepeatSubstring.cpp
@@ -1,27 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class NoRepeatSubstring {
+class Solution {
 public:
-    static int noRepeatSubstring(string s) {
-        int windowStart = 0;
-        int maxLength = INT_MIN;
-        map<char, int> indexMap;
-        if(s.size() == 0) return 0;
-        
-        for(int windowEnd = 0; windowEnd < s.size(); windowEnd++) {
-            char currChar = s[windowEnd];
-            if(indexMap.find(currChar) != indexMap.end()) {
-                windowStart = max(windowStart, indexMap[currChar] + 1);
-            }
-            indexMap[currChar] = windowEnd;
-            maxLength = max(maxLength, windowEnd - windowStart + 1);
+        int lengthOfLongestSubstring(string s) {
+        int length=0 , maxlength=0,j=0;
+        map<char ,int> mp;
+        for(int i=0 ;i<s.size(); i++){
+            mp[s[i]]++;
+            length++;
+                while(mp[s[i]]>1){
+                    mp[s[j++]]--;
+                    length--;
+                }
+            maxlength = max(maxlength,length);
         }
-        return maxLength;
-    } 
+        return maxlength;
+    }
 };
 
 int main() {
     string s = "abcabcbb";
-    cout << NoRepeatSubstring::noRepeatSubstring(s) << endl;
+    cout << lengthOfLongestSubstrin(s) << endl;
 }
